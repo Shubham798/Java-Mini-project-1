468c2f5f50ebddd6ad45d2bc8ddbee028162f121
Shubham798 <42290145+Shubham798@users.noreply.github.com>
2019-10-05 13:54:21 +0530

    Add files via upload

--- MiniProject/src/miniproject/javafiles/RestMenu.java
+++ MiniProject/src/miniproject/javafiles/RestMenu.java
@@ -1,30 +1,12 @@
 package miniproject.javafiles;
 
-import miniproject.databasefiles.getMenu;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-
 public class RestMenu {
-    String []dishNames;
-    int [] dishPrices;
+    String []dishNames=new String[10];
+    int []dishPrices=new int[10];
 
-    void getMenuFromDB() throws SQLException, ClassNotFoundException {
-        getMenu gm = new getMenu();
-        ArrayList <ArrayList> al = new ArrayList();
-        al = gm.getMenuFromtable();
-        ArrayList m = new ArrayList();
-        ArrayList<Integer> p = new ArrayList();
-        m = al.get(0);
-        p = al.get(1);
-        dishPrices = new int[p.size()];
-        dishNames = new String[m.size()];
-
-        for (int i = 0; i < p.size(); i++) {
-            dishPrices[i] = p.get(i);
-            dishNames[i] = m.get(i).toString();
-        }
-
+    void getMenuFromDB(){
+        dishNames=new String[]{"a","b","c","d","e","f","g","h","i","j"};//ACCEPT FROM THE DB
+        dishPrices=new int[]{0,9,8,7,6,5,4,3,2,1};
     }
     String[] getDishNames(){
         return dishNames;
